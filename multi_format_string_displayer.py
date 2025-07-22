f70878eacb12b600800b381850838242f422224e
Mukhtar <mdmukhtarmohsin@gmail.com>
2025-07-22 17:18:27 +0530

    multi_format_string_displayer

---
+++ multi_format_string_displayer.py
@@ -0,0 +1,15 @@
+name="Alice"
+age=28
+city="Delhi"
+print(f"My name is {name}, I am {age} years old and I live in {city}")
+print("My name is {name}, I am {age} years old and I live in {city}".format(name=name, age=age, city=city))
+print("My name is {0}, I am {1} years old and I live in {2}".format(name, age, city))
+print("My name is %s, I am %d years old and I live in %s" % (name, age, city))
+
+
+
+
+
+
+
+
